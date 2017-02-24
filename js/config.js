946a4f4e135517833aeadcf3324d85f4081c526c
Donaven15s <Donaven15s@gmail.com>
2017-02-24 06:26:42 -0800

    ElementalGaming loading screen

---
+++ js/config.js
@@ -0,0 +1,110 @@
+/*    _           _    _                     _ 
+     | |         | |  | |                   | |
+  ___| | ___  ___| | _| |     ___   __ _  __| |
+ / __| |/ _ \/ _ \ |/ / |    / _ \ / _` |/ _` |
+ \__ \ |  __/  __/   <| |___| (_) | (_| | (_| |
+ |___/_|\___|\___|_|\_\______\___/ \__,_|\__,_|
+    version 2.08
+*/
+
+/*URL for the server logo
+Leave it blank to use text instead
+(Put the files in the images folder)*/
+var l_serverImage = "logo.png";
+
+/*Custom server name configuration
+Leave it blank for automatic server name*/
+var l_serverName = "ElementalGaming";
+
+/*Use a video for the background?
+Otherwise it will just use images*/
+var l_bgVideo = false;
+
+/*Video background
+Avaiable options:
+"touhou04.webm"
+"touhou05.webm"
+"touhou06.webm"
+
+You can add custom backgrounds
+Just make sure it is at webm VP8 format 
+(Put the files in the backgrounds/videos folder)*/
+var l_background = "glimmering.webm";
+
+/*(Image-background only)
+Place one or more images
+Multiple images will automatically shuffle between them
+(Put the files in the backgrounds/images folder)*/
+var l_bgImages = [
+	"touhou01.jpg",
+	"touhou02.jpg",
+	"touhou03.jpg",
+	"touhou04.jpg",
+	"touhou05.jpg",
+	"touhou06.jpg",
+];
+
+/*Random background images order?*/
+var l_bgImagesRandom = true;
+
+/*(Image-background only)
+Delay between background images changes
+in milliseconds*/
+var l_bgImageDuration = 5000;
+
+/*(Image-background only)
+Image background fade velocity
+in milliseconds*/
+var l_bgImageFadeVelocity = 2000;
+
+/*Enable background overlay?*/
+var l_bgOverlay = true;
+
+/*Background darkening amount
+0 for none (0%), 100 for pitch black (100%)*/
+var l_bgDarkening = 50;
+
+/*Music player
+'true' or 'false' to enable music in the background*/
+var l_music = true;
+
+/*Display actual song's name?*/
+var l_musicDisplay = true;
+
+/*Music playlist
+Place how much .ogg songs you want
+(Put the files in the songs folder)*/
+var l_musicPlaylist = [
+	{youtube: "gGpxSwAd-R8", name: "Element - Ressurection [Goblin Promotion]"},
+	{youtube: "bfIXJMYaofI", name: "Netsiah - Two Lives"},
+	{youtube: "Q1DvVzKXktY", name: "Uppermost - Beautiful Light"},
+];
+
+/*Random music order?*/
+var l_musicRandom = true;
+
+/*Music volume
+Choose a value between 0 and 100*/
+var l_musicVolume = 10;
+
+/*Enable custom messages?*/
+var l_messagesEnabled = true;
+
+/*Enter your custom messages below*/
+var l_messages = [
+	"Have Fun And Enjoy ElementalGaming",
+	"Thanks for joining us!",
+    "You are now connecting to ElementalGaming!",
+	"Be Sure to Give A Special Thanks to Captian And Vengaming For Hosting The Sever"
+];
+
+/*Random message order?*/
+var l_messagesRandom = true;
+
+/*Delay between message changes
+in milliseconds*/
+var l_messagesDelay = 5000;
+
+/*Messages fade time
+in milliseconds*/
+var l_messagesFade = 1000;
